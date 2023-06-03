e112bcfd95e756b98fb750d86d83f03647cac24c
zmk161 <Kevin321an@gmail.com>
2023-06-03 11:55:06 -0400

    remove the default launcher icon and correct the project id

--- app/src/main/java/com/flybit/concierge5compose/Constant.kt
+++ app/src/main/java/com/flybit/concierge5compose/Constant.kt
@@ -1,4 +1,4 @@
 package com.flybit.concierge5compose
-val project_id = "123"
+val project_id = "35F6A9F5-579B-4229-815C-7D994CD50F9C"
 val gateway_url = "https://api.demo.flybits.com"
 val webService_url = ""
