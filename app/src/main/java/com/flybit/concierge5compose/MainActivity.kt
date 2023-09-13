71008e97fa6bc481a64dee46d7d400df68cc2aeb
Artur Shamsi <artur.shamsi@flybits.com>
2023-09-13 13:45:48 -0400

    attempt to implement broadcast receiver

--- app/src/main/java/com/flybit/concierge5compose/MainActivity.kt
+++ app/src/main/java/com/flybit/concierge5compose/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.flybit.concierge5compose
 
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -15,27 +19,35 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.viewinterop.AndroidViewBinding
-import androidx.fragment.app.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.flybit.concierge5compose.ui.theme.Concierge5ComposeTheme
-import com.flybits.commons.library.api.FlybitsManager
 import com.flybits.commons.library.api.results.callbacks.BasicResultCallback
 import com.flybits.commons.library.exceptions.FlybitsException
 import com.flybits.commons.library.logging.VerbosityLevel
 import com.flybits.concierge.Concierge
+import com.flybits.concierge.ConciergeConstants
+import com.flybits.concierge.ConciergeConstants.BROADCAST_CONCIERGE_EVENT
 import com.flybits.concierge.FlybitsConciergeConfiguration
+import com.flybits.concierge.enums.ConciergeCustomerStatus
 import com.flybits.concierge.enums.ConciergeOptions
 import com.flybits.concierge.enums.ConciergeParams
 import com.flybits.concierge.enums.Container
 import com.flybits.concierge.enums.ContentStyle
-import com.flybits.flybitscoreconcierge.conciergeidps.connect
 import com.flybits.flybitscoreconcierge.idps.AnonymousConciergeIDP
 
 fun Context.getActivity(): AppCompatActivity? = when (this) {
@@ -62,12 +74,43 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun ComposableBroadcastReceiver(
+    action: String,
+    event: (context: Context, intent: Intent?) -> Unit,
+) {
+    // Grab the current context in this part of the UI tree
+    val context = LocalContext.current
 
+    // Safely use the latest event lambda passed to the function
+    val currentOnSystemEvent by rememberUpdatedState(event)
+
+    // If either context or systemAction changes, unregister and register again
+    DisposableEffect(context, action) {
+        val intentFilter = IntentFilter(action)
+        val broadcast = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d("MainActivity", "onReceive called.")
+                context?.let {
+                    currentOnSystemEvent(it, intent)
+                }
+            }
+        }
+
+        context.registerReceiver(broadcast, intentFilter)
+
+        // When the effect leaves the Composition, remove the callback
+        onDispose {
+            context.unregisterReceiver(broadcast)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
     Column() {
         val context = LocalContext.current
         val activity = context.getActivity()
-
+        var broadcastIntent: Intent? = null
         Row() {
             Button(onClick = {
                 println("test315 button on click")
@@ -90,10 +133,6 @@ fun Greeting(name: String) {
                 Text(text = "disconnect from Flybit")
             }
         }
-
-
-
-
         activity?.supportFragmentManager?.let { fragmentManager ->
             /***
              * Example to display a Banner view
@@ -118,10 +157,59 @@ fun Greeting(name: String) {
              * Example to display a C5 view
              */
             FragmentContainer(modifier = Modifier, fragmentManager = fragmentManager, commit = {
-                add(it, C5Fragment(context))
+                replace(it, C5Fragment(context))
             })
         }
 
+        ComposableBroadcastReceiver(BROADCAST_CONCIERGE_EVENT, ::handleOnReceive)
+    }
+}
+
+fun handleOnReceive(context: Context, intent: Intent?) {
+    // For example getting a dummy value
+    val value = intent?.extras?.getString(ConciergeConstants.CONCIERGE_IDENTIFIER)
+    val actionableLink = intent?.extras?.getString(ConciergeConstants.ACTIONABLE_URL)
+    val customerStatus: ConciergeCustomerStatus? = intent?.extras?.getParcelable(ConciergeConstants.CONCIERGE_AUTHENTICATION_STATUS)
+    val analyticsType = intent?.extras?.getString(ConciergeConstants.ANALYTICS_TYPE)
+    val contentData: HashMap<String, Any>? = intent?.extras?.getSerializable(ConciergeConstants.CONTENT_DATA)
+            as HashMap<String, Any>?
+    val contentInfo: HashMap<String, String>? = intent?.extras?.getSerializable(
+        ConciergeConstants
+            .CONTENT_INFO) as HashMap<String, String>?
+    val hasContent = intent?.extras?.getString(ConciergeConstants.HAS_CONTENT)
+
+    actionableLink?.let {
+        Log.d("BroadcastActionLink", value.toString() + " / " + actionableLink.toString())
+    }
+    analyticsType?.let {
+        Log.d("BroadcastAnalytics", value.toString() + " / " +
+                it.toString() + " / " + contentData.toString() + " / " + contentInfo.toString())
+    }
+    customerStatus?.let {
+        Log.d("BroadcastStatus", value.toString() + " / " + it.toString())
+    }
+    hasContent?.let {
+        Log.d("BroadcastHasContent", value.toString() + " / " + it)
+    }
+
+    actionableLink?.let {
+        context.let { ctx ->
+            Concierge.handleActionableLink(
+                ctx,
+                Uri.parse(actionableLink),
+                conciergeOptions = arrayListOf(ConciergeOptions.Identifier("123")),
+                requestEvents = ConciergeParams.RequestEvents()/*,
+                    ConciergeTheme.FileName("custom_theming1.json")*/)
+                ?.let { fragment ->
+                    context.getActivity()?.supportFragmentManager?.let {fm ->
+                        FragmentContainer(modifier = Modifier, fragmentManager = fm, commit = {
+                            add(it, fragment)
+                        })
+                    }
+                } ?: run {
+                Log.i("", "Returns null")
+            }
+        }
     }
 }
 
@@ -173,7 +261,7 @@ fun c4Fragment(applicationContext: Context): Fragment {
 fun C5Fragment(applicationContext: Context): Fragment {
     return Concierge.fragment(applicationContext,
             Container.Configured,
-            arrayListOf(),
+            arrayListOf(ConciergeParams.RequestEvents()),
             arrayListOf(ConciergeOptions.DisplayNavigation("c5 content showing here"),
                     ConciergeOptions.Settings,
                     ConciergeOptions.Notifications)
@@ -183,9 +271,9 @@ fun C5Fragment(applicationContext: Context): Fragment {
 fun init(context: Context) {
     Concierge.setLoggingVerbosity(VerbosityLevel.ALL)
     val configurationBuilder = FlybitsConciergeConfiguration.Builder(context)
-            .setProjectId("35F6A9F5-579B-4229-815C-7D994CD50F9C")
+            .setProjectId("F1BFE6D1-0DB6-4863-A3E5-56434C2075FB")
             .setGatewayUrl("https://api.demo.flybits.com")
-            .setWebService("localhost:3000")
+            .setWebService("https://static-files-concierge.tdunmndb289.flybits.com/latest")
             .build()
     Concierge.configure(configurationBuilder, emptyList(), context)
 }
